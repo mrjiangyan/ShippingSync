10c3ae9de73924f82c4be09f179072c098f57b17
Steven <mrjiangyan@hotmail.com>
2017-03-01 22:13:03 +0800

    项目结构提交

---
+++ ShippingSyncServer/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace ShippingSyncServer.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
