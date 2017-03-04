74be5f88660ccea0b65c58aba1738505f2562136
Steven <mrjiangyan@hotmail.com>
2017-03-04 16:30:39 +0800

    将日志拆分为每小时记录一个，将不同的日志差分不同的文件夹记录

---
+++ Utilities/Utils/LogUtility.cs
@@ -0,0 +1,101 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Utils
+{
+    public class LogUtility
+    {
+        private static ILog _logdebug = LogManager.GetLogger("logdebug");
+        private static ILog _loginfo = LogManager.GetLogger("loginfo");
+        private static ILog _logwarn = LogManager.GetLogger("logwarn");
+        private static ILog _logerror = LogManager.GetLogger("logerror");
+        private static ILog _logfatal = LogManager.GetLogger("logfatal");
+
+
+        public static void Debug(string msg)
+        {
+            StackFrame sf = new StackFrame(1);
+            msg = string.Format("{0}.{1}:{2}", sf.GetMethod().ReflectedType.FullName, sf.GetMethod().Name, msg);
+            _logdebug.Debug(msg);
+        }
+
+
+        public static void Debug(string msg, Exception e)
+        {
+            StackFrame sf = new StackFrame(1);
+            msg = string.Format("{0}.{1}:{2}", sf.GetMethod().ReflectedType.FullName, sf.GetMethod().Name, msg);
+            _logdebug.Debug(msg, e);
+        }
+
+
+        public static void Info(string msg)
+        {
+            StackFrame sf = new StackFrame(1);
+            msg = string.Format("{0}.{1}:{2}", sf.GetMethod().ReflectedType.FullName, sf.GetMethod().Name, msg);
+            _loginfo.Info(msg);
+        }
+        public static void Info(string msg, Exception e)
+        {
+            StackFrame sf = new StackFrame(1);
+            msg = string.Format("{0}.{1}:{2}", sf.GetMethod().ReflectedType.FullName, sf.GetMethod().Name, msg);
+            _loginfo.Info(msg, e);
+        }
+
+
+
+
+        public static void Warn(string msg)
+        {
+            StackFrame sf = new StackFrame(1);
+            msg = string.Format("{0}.{1}:{2}", sf.GetMethod().ReflectedType.FullName, sf.GetMethod().Name, msg);
+            _logwarn.Warn(msg);
+        }
+        public static void Warn(string msg, Exception e)
+        {
+            StackFrame sf = new StackFrame(1);
+            msg = string.Format("{0}.{1}:{2}", sf.GetMethod().ReflectedType.FullName, sf.GetMethod().Name, msg);
+            _logwarn.Warn(msg, e);
+        }
+
+
+        public static void Error(string msg)
+        {
+            StackFrame sf = new StackFrame(1);
+            msg = string.Format("{0}.{1}:{2}", sf.GetMethod().ReflectedType.FullName, sf.GetMethod().Name, msg);
+            _logerror.Error(msg);
+        }
+
+
+        public static void Error(string msg, Exception e)
+        {
+            StackFrame sf = new StackFrame(1);
+            msg = string.Format("{0}.{1}:{2}", sf.GetMethod().ReflectedType.FullName, sf.GetMethod().Name, msg);
+            _logerror.Error(msg, e);
+        }
+
+
+        public static void Fatal(string msg)
+        {
+            StackFrame sf = new StackFrame(1);
+            msg = string.Format("{0}.{1}:{2}", sf.GetMethod().ReflectedType.FullName, sf.GetMethod().Name, msg);
+            _logfatal.Fatal(msg);
+        }
+
+
+        public static void Fatal(string msg, Exception e)
+        {
+            StackFrame sf = new StackFrame(1);
+            msg = string.Format("{0}.{1}:{2}", sf.GetMethod().ReflectedType.FullName, sf.GetMethod().Name, msg);
+            _logfatal.Fatal(msg, e);
+        }
+
+
+    }
+}
